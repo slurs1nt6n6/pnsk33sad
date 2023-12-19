5f0082b7c584dc1f1b034661184cb1d56268f891
slurs1nt6n6 <serg195999@mail.ru>
2023-12-19 23:01:35 +0700

    =(

--- js/main.js
+++ js/main.js
@@ -1,63 +1,6 @@
 let eventBus = new Vue()
 
-Vue.component('cards-kanban', {
-    template:`
-    <div>
-        <fill></fill>
-        <div id="columns">
-            <column1 :column1="column1"></column1>
-            <column2 :column2="column2"></column2>
-            <column3 :column3="column3"></column3>
-            <column4 :column4="column4"></column4>
-        </div>
-    </div>
-    `,
-    data() {
-        return {
-            column1:[],
-            column2:[],
-            column3:[],
-            column4:[],
-            showCard: true,
-        }
-    },
-    methods:{
 
-    },
-    mounted() {
-        eventBus.$on('card-create', card => {
-            this.column1.push(card)
-        })
-        eventBus.$on('moving1', card => {
-            this.column2.push(card)
-            this.column1.splice(this.column1.indexOf(card), 1)
-
-        })
-        eventBus.$on('moving2', card => {
-            this.column3.push(card)
-            this.column2.splice(this.column2.indexOf(card), 1)
-        })
-
-        eventBus.$on('moving3-2', card => {
-            this.column2.push(card)
-            this.column3.splice(this.column3.indexOf(card), 1)
-            card.dateE = new Date().toLocaleDateString()
-        })
-
-        eventBus.$on('moving3-4', card => {
-            this.column4.push(card)
-            this.column3.splice(this.column3.indexOf(card), 1)
-            card.dateE = new Date().toLocaleDateString()
-            card.dateE = card.dateE.split('.').reverse().join('-')
-            console.log(card)
-            if (card.dateE > card.dateD){
-                card.inTime = false
-            }
-        })
-    }
-})
-
-//asd
 let app = new Vue({
     el:'#app',
     data:{
