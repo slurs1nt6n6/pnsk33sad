658c3e0cc3805dfb8fd69edb5a3d58f7812056d7
slurs1nt6n6 <serg195999@mail.ru>
2023-12-19 23:17:34 +0700

    x4=(column1

--- js/main.js
+++ js/main.js
@@ -61,12 +61,7 @@ Vue.component('fill', {
     template:`
     <div>
     <div>
-        <button v-if="!show" @click="openModal">Добавьте задачу</button>
-        <div id="form" v-if="show" class="modal-shadow">
-            <div class="modal">
-                <div class="modal-close" @click="closeModal">&#10006;</div>
-                <h3>Заполните карточку задачи</h3>
-                <form @submit.prevent="onSubmit">
+        <button v-if="!show" @click="openModal">Дta
                     <p class="pForm">Введите заголовок: 
                         <input required type="text" v-model="title" maxlength="30" placeholder="Заголовок">
                     </p>
@@ -118,6 +113,69 @@ Vue.component('fill', {
             this.show = true
         }
     }
+})
+Vue.component('column1', {
+    props:{
+        card: {
+            type: Object,
+            required: true
+        },
+        column1: {
+            type: Array,
+            required: true
+        },
+    },
+    template:`
+    <div class="column">
+        <h3>Запланированные задачи</h3>
+        <div class="card" v-for="card in column1">
+            <ul>
+                <li class="title"><b>Заголовок:</b> {{ card.title }}</li>
+                <li><b>Описание задачи:</b> {{ card.description }}</li>
+                <li><b>Дата дедлайна:</b> {{ card.dateD }}</li>
+                <li><b>Дата создания:</b> {{ card.dateC }}</li>
+                <li v-if="card.dateL"><b>Дата последних изменений</b>{{ card.dateL }}</li>
+                <button @click="deleteCard(card)">Удалить</button>
+                <button @click="updateC(card)">Изменить</button>
+                <div class="change" v-if="card.updateCard">
+                    <form @submit.prevent="updateTask(card)">
+                        <p>Введите заголовок: 
+                            <input type="text" v-model="card.title" maxlength="30" placeholder="Заголовок">
+                        </p>
+                        <p>Добавьте описание задаче: 
+                            <textarea v-model="card.description" cols="20" rows="5"></textarea>
+                        </p>
+                        <p>Укажите дату окончания срока: 
+                            <input type="date" v-model="card.dateD">
+                        </p>
+                        <p>
+                             <input class="button" type="submit" value="Изменить карточку">
+                        </p>
+                    </form>
+                </div>
+             </ul>
+            <button @click="moving(card)">--></button>
+        </div>
+    </div>
+    `,
+    methods: {
+        deleteCard(card){
+            this.column1.splice(this.column1.indexOf(card), 1)
+        },
+        updateC(card){
+            card.updateCard = true
+            console.log(card.updateCard)
+        },
+        updateTask(card){
+            this.column1.push(card)
+            this.column1.splice(this.column1.indexOf(card), 1)
+            card.dateL = new Date().toLocaleString()
+            return card.updateCard = false
+        },
+        moving(card){
+            eventBus.$emit('moving1', card)
+        }
+    },
 })
 
 let app = new Vue({
